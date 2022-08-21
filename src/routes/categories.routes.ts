a3a6867832e4cf1996b81fabd777272f5bacef2d
Lucas Thomaz <lucasthomazr@hotmail.com>
2022-08-20 23:54:27 -0300

    Finishes import feature for categories route

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,13 @@
 import { Request, Response, Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/Categories/createCategory';
+import { importCategoriesController } from '../modules/cars/useCases/Categories/importCategories';
 import { listCategoriesController } from '../modules/cars/useCases/Categories/listCategories';
+
+const upload = multer({
+  dest: './temp',
+});
 
 const categoriesRoutes = Router();
 
@@ -12,5 +18,13 @@ categoriesRoutes.post('/', (request: Request, response: Response) => {
 categoriesRoutes.get('/', (request: Request, response: Response) => {
   return listCategoriesController.handle(request, response);
 });
+
+categoriesRoutes.post(
+  '/import',
+  upload.single('file'),
+  (request: Request, response: Response) => {
+    return importCategoriesController.handle(request, response);
+  },
+);
 
 export { categoriesRoutes };
